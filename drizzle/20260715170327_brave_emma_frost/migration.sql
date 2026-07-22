5de9ce6a387513bde1487b5c2a7687653ae6f94a
Gabriel <Imre>
2026-07-22 18:07:25 +0300

    fix drizzle versioning; new migrations

--- drizzle/20260715170327_brave_emma_frost/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "products" ADD COLUMN "price" numeric NOT NULL;--> statement-breakpoint
-ALTER TABLE "products" ADD COLUMN "currency" varchar NOT NULL;
--- drizzle/20260720161849_woozy_omega_flight/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" ADD COLUMN "handle" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ADD CONSTRAINT "user_handle_key" UNIQUE("handle");
