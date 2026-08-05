6204b9555937308ed0ed64d76b1d8dacd3d6e4f0
Gabriel <Imre>
2026-08-05 17:51:37 +0300

    add image wrapper for testing; add preloading / eager images to storefront page and product carousel

--- components/product-images.tsx
+++ components/product-images.tsx
@@ -2,9 +2,9 @@
 
 import { useState, useTransition } from "react"
 import { useRouter } from "next/navigation"
-import Image from "next/image"
 import { X } from "lucide-react"
 
+import { Image } from "@/components/image"
 import { UploadDropzone } from "@/lib/client/uploadthing"
 import { deleteProductImageAction } from "@/lib/actions/products"
 import { MAX_PRODUCT_IMAGES } from "@/lib/schemas/product"
